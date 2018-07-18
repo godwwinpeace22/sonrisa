9f98b088cb2a2d10f18d5dde8a6b5f1f7538a681
godwin gabriel <godwinpeace22@gmail.com>
2018-07-18 16:39:58 +0100

    removed login and register routes from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
 Vue.use(Router)
 export default new Router({
     mode:'hash',
@@ -11,16 +9,6 @@ export default new Router({
             path:'/',
             name:'home',
             component:Home
-        },
-        {
-            path:'/login',
-            name:'login',
-            component:Login
-        },
-        {
-            path:'/register',
-            name:'register',
-            component:Register
         }
         
     ]
