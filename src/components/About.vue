f1276e3e938fb5e17e850e378499eb08f3dbf255
godwin gabriel <godwinpeace22@gmail.com>
2018-07-18 18:41:21 +0100

    resized many images

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,6 +5,20 @@
 			<b-row>
 				<b-col>
 					<h3>About me</h3>
+					<b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+						<template slot="header">
+							Bootstrap Vue
+						</template>
+						<template slot="lead">
+							This is a simple hero unit, a simple jumbotron-style component for
+							calling extra attention to featured content or information.
+						</template>
+						<hr class="my-4">
+						<p>
+							It uses utility classes for typography and spacing to space content
+							out within the larger container.
+						</p>
+					</b-jumbotron>
 				</b-col>
 			</b-row>
 		</b-container>
