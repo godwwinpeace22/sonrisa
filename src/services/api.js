e78cd9b518f53d2d552f7a089ae9a36c23e1ea30
godwin gabriel <godwinpeace22@gmail.com>
2018-08-17 17:49:53 +0100

    sv url changed

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default()=>{
     return axios.create({
-        baseURL: 'http:/' +'/localhost:3000' //'https:/' + '/secure-citadel-58191.herokuapp.com'
+        baseURL: 'https:/' + '/secure-citadel-58191.herokuapp.com'
     })
 }
