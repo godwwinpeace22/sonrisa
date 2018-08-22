af2771558f47dcb8fdd01a3254d0f801c7c65244
godwin gabriel <godwinpeace22@gmail.com>
2018-08-22 17:10:08 +0100

    test b

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default()=>{
     return axios.create({
-        baseURL:"https:/" + "/secure-citadel-58191.herokuapp.com" //"http:/" + "/localhost:3000" 
+        baseURL:"https:/" + "/secure-citadel-58191.herokuapp.com/" //"http:/" + "/localhost:3000" 
     })
 }
